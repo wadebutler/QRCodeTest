505440bf7ec74dbd4db4e61bfdbcf4cec6499afb
Wade Butler <wadebutler@MacBook-Pro.local>
2022-07-21 17:08:36 -0400

    qr test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import QRCode from 'qrcode';
+import { QRCodeSVG } from 'qrcode.react';
 
 function App() {
+  const bigString = 'hello to the many hats team!hello to the many hats team!hello to the many hats team!hello to the many hats team!hello to the many hats team!hello to the many hats team!hello to the' //180
+  const smlString = 'hello to the many hats team!' //28
+
+  const [src, setSrc] = useState()
+  const [testObj, setTestObj] = useState({key: 7967, name: ''})
+
+  useEffect(() => {
+    QRCode.toDataURL(JSON.stringify(testObj)).then((data) => {
+      setSrc(data);
+    })
+  }, [])
+
+  const handleInput = (e) => {
+    setTestObj({key: testObj.key, name: e.target.value})
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+      <textarea style={{marginTop: 20, marginBottom: 20, height: 150, width: 300}} onChange={(event) => handleInput(event)} />
+
+      <QRCodeSVG size={200} value={JSON.stringify(testObj)} renderAs="svg" />,
     </div>
   );
 }
