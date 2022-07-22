5d5d7c1ae4a1fa0a9eb8acbdbdd321b71a8a6a84
Wade Butler <wadebutler@MacBook-Pro.local>
2022-07-22 14:00:10 -0400

    update

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import QRCode from 'qrcode';
 import { QRCodeSVG } from 'qrcode.react';
 
 function App() {
-  const bigString = 'hello to the many hats team!hello to the many hats team!hello to the many hats team!hello to the many hats team!hello to the many hats team!hello to the many hats team!hello to the' //180
-  const smlString = 'hello to the many hats team!' //28
-
   const [src, setSrc] = useState()
   const [testObj, setTestObj] = useState({key: 7967, name: ''})
 
@@ -21,10 +18,11 @@ function App() {
   }
 
   return (
-    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-      <textarea style={{marginTop: 20, marginBottom: 20, height: 150, width: 300}} onChange={(event) => handleInput(event)} />
+    <div className='container' style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+      <textarea className='display-none' style={{marginTop: 20, marginBottom: 20, height: 150, width: 300}} onChange={(event) => handleInput(event)} />
 
-      <QRCodeSVG size={200} value={JSON.stringify(testObj)} renderAs="svg" />,
+      
+      <QRCodeSVG size={200} value={JSON.stringify(testObj)} renderAs="svg" />
     </div>
   );
 }
